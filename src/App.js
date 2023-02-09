8be63ae5d1fed004b2ae20b77aaa341a14c8b868
choi ha young <gkdudk15@naver.com>
2023-02-09 22:17:00 +0900

    feat: 입문 주차 퀴즈

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [toDo, setToDo] = useState([]);
+    const [toDoTitle, setToDoTitle] = useState('');
+
+    const clickHandler = () => {
+        let copy = [toDoTitle, ...toDo];
+        setToDo(copy);
+        setToDoTitle('');
+    };
+
+    return (
+        <div className="App">
+            <div className="input-box">
+                <input
+                    type="text"
+                    placeholder="자바스크립트를 배워봅시다"
+                    onChange={(e) => setToDoTitle(e.target.value)}
+                    value={toDoTitle}
+                />
+                <button onClick={clickHandler}>추가하기</button>
+            </div>
+            <div className="list-container">
+                <h3 className="list-title">Todo List</h3>
+                <div className="list-cards">
+                    {toDo.map((item, idx) => (
+                        <div className="todo-card" key={idx}>
+                            {item}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
