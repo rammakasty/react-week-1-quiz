01b8a20058d39710edf5243a895d6b12f9f68a57
choi ha young <gkdudk15@naver.com>
2023-02-22 12:14:09 +0900

    action value counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { PLUS_ONE } from './redux/modules/counter';
+import { MINUS_ONE } from './redux/modules/counter';
 
 function App() {
     const counter = useSelector((state) => {
         return state.counter;
     });
 
+    const dispatch = useDispatch();
+
     console.log('counter', counter);
 
-    return <div>현재카운트:</div>;
+    return (
+        <div>
+            현재카운트: {counter.number}
+            <button
+                onClick={() => {
+                    //  +1을 해주는 로직을 써주면 된다.
+                    dispatch({
+                        type: PLUS_ONE
+                    });
+                }}
+            >
+                +
+            </button>
+            <button
+                onClick={() => {
+                    //  +1을 해주는 로직을 써주면 된다.
+                    dispatch({
+                        type: MINUS_ONE
+                    });
+                }}
+            >
+                -
+            </button>
+        </div>
+    );
 }
 
 export default App;
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,9 +1,19 @@
+export const PLUS_ONE = "counter/PLUS_ONE"
+export const MINUS_ONE = "counter/MINUS_ONE"
 const inintialState = {
     number: 0,
 };
 
 const counter = (state = inintialState, action) => {
     switch (action.type) {
+        case PLUS_ONE:
+            return {
+                number: state.number + 1
+            }
+            case MINUS_ONE:
+                return {
+                    number: state.number - 1
+                }
         default:
             return state;
     }
